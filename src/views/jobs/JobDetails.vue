ec0db41f6bb6e890b1f027cb76cd7fea14facd19
shovon26 <djshovon12@gmail.com>
2023-02-28 15:59:36 +0600

    Second Commit

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,16 +1,35 @@
 <template>
-  <h1>Job details page</h1>
-  <p>The job id :: {{ id }}</p>
+    <div v-if="job">
+        <h1>Job information page</h1>
+        <p>Job Title: {{ job.title }}</p>
+        <p>Job Details: {{ job.details }}</p>
+        <p>The job id :: {{ id }}</p>
+        <p>{{ print }}</p>
+    </div>
+    <div v-else>
+        <p>Details is loading...</p>
+    </div>
 </template>
 
 <script>
 export default {
     props: ['id'],
-    // data(){
-    //     return {
-    //         id: this.$route.params.id
-    //     }
-    // }
+    data() {
+        return {
+            job: null
+        }
+    },
+    mounted() {
+        fetch('http://localhost:3000/jobs/' + this.id)
+            .then(res => res.json())
+            .then(data => this.job = data)
+            .catch(err => console.log(err.message))
+    },
+    computed: {
+        print(){
+            console.log('details is shown')
+        }
+    }
 }
 </script>
 
--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,22 +1,31 @@
 <template>
   <h1>Show Jobs list</h1>
-  <div v-for="job in jobs" :key="job.id" class="job">
-    <router-link :to="{ name: 'JobDetails', params: {id: job.id }}">
-      <h2> {{ job.title }} </h2>
-    </router-link>
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id" class="job">
+        <router-link :to="{ name: 'JobDetails', params: {id: job.id }}">
+        <h2> {{ job.title }} </h2>
+        </router-link>
+    </div>
   </div>
+
+  <div v-else>
+    <p>loading jobs...</p>
+  </div>
+  
 </template>
 
 <script>
 export default {
     data(){
         return {
-            jobs: [
-                {title: 'Ninja UX Designer' , id:1, details: 'ux designer!'},
-                {title: 'Ninja QA Engineer' , id:2, details: 'qa engineer!'},
-                {title: 'Ninja Software Engineer' , id:3, details: 'software engineer'}
-            ]
+            jobs: []
         }
+    },
+    mounted(){
+        fetch('http://localhost:3000/jobs')
+            .then(res => res.json())
+            .then(data => this.jobs = data)
+            .catch(err => console.log(err.message))
     }
 }
 </script>
